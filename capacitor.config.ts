8cffc29318104cdee08b27aebb80edaf85a93ce8
Chavadios29 <2021371002@uteq.edu.mx>
2025-03-04 21:35:18 -0600

    Primer Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rent4students',
+  webDir: 'www'
+};
+
+export default config;
