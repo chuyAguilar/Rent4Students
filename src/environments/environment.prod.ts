ca602eabb990f91b3a58949c7572824516391a4b
jesus alfredo auilar cruz <aguilarcruz@outlook.es>
2025-03-06 14:05:01 -0600

    agregue credenciales para firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbMTTzlHw1AZhVtsiijyMQM1Ih1Ijxl0c",
+    authDomain: "rent4students-2f66b.firebaseapp.com",
+    projectId: "rent4students-2f66b",
+    storageBucket: "rent4students-2f66b.firebasestorage.app",
+    messagingSenderId: "108815976232",
+    appId: "1:108815976232:web:2fe1001fce3e23e87f321d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbMTTzlHw1AZhVtsiijyMQM1Ih1Ijxl0c",
+    authDomain: "rent4students-2f66b.firebaseapp.com",
+    projectId: "rent4students-2f66b",
+    storageBucket: "rent4students-2f66b.firebasestorage.app",
+    messagingSenderId: "108815976232",
+    appId: "1:108815976232:web:2fe1001fce3e23e87f321d"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
