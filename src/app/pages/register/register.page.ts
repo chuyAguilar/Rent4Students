314d246ffa355ad9f10e578c80d91e58f31e45e2
jesus alfredo auilar cruz <aguilarcruz@outlook.es>
2025-03-13 13:37:47 -0600

    agregue login y registro con firebase

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,30 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { IonicModule, NavController } from '@ionic/angular';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  standalone: false
+  standalone: true,  // ✅ Indica que es un componente standalone
+  imports: [CommonModule, IonicModule, FormsModule] // ✅ Importar módulos necesarios
 })
 export class RegisterPage {
-  userType: string = '';
   user: string = '';
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
+  userType: string = '';
 
-  constructor(private navCtrl: NavController) {}
+  constructor(private navCtrl: NavController, private authService: AuthService) {}
 
-  register() {
-    console.log('User:', this.user);
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-    console.log('Confirm Password:', this.confirmPassword);
-    console.log('UserType:', this.userType);
-  
+  async register() {
     if (!this.user || !this.email || !this.password || !this.confirmPassword || !this.userType) {
       alert('Todos los campos son obligatorios');
+      return;
+    }
+  
+    if (this.password.length < 6) {
+      alert('La contraseña debe tener al menos 6 caracteres');
       return;
     }
   
@@ -33,16 +36,16 @@ export class RegisterPage {
       return;
     }
   
-    // Guardar en localStorage
-    localStorage.setItem('user', JSON.stringify({
-      user: this.user,
-      email: this.email,
-      password: this.password,
-      userType: this.userType
-    }));
-  
-    //alert('Registro exitoso, ahora puedes iniciar sesión');
-    this.navCtrl.navigateForward('/login');
+    try {
+      const userCredential = await this.authService.register(this.email, this.password, this.userType, this.user);
+      if (userCredential) {
+        console.log('Usuario registrado:', userCredential);
+        alert('Registro exitoso');
+        this.navCtrl.navigateForward('/login');
+      }
+    } catch (error) {
+      alert('Error en el registro: ' + (error as any).message);
+    }
   }
   
 }
